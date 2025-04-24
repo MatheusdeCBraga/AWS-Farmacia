19857b3af0d48dbebc3ffb3bbdccb9c18fdf8746
MatheusdeCBraga <matheus.braga@gsuite.iff.edu.br>
2025-04-23 21:40:19 -0300

    envio 2

--- git/AWS/Farmacia/scripts/ecs/unix/build.sh
+++ git/AWS/Farmacia/scripts/ecs/unix/build.sh
@@ -1,6 +1,5 @@
-ECR_REGISTRY="855414917031.dkr.ecr.us-east-1.amazonaws.com/aws-farmacia"
+ECR_REGISTRY="855414917031.dkr.ecr.us-east-1.amazonaws.com"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
 docker build -t aws_farmacia .
 docker tag aws_farmacia:latest $ECR_REGISTRY:latest
 docker push $ECR_REGISTRY:latest
-ß
